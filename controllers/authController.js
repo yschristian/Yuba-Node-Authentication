a0bc0ffe2077d3cc211d3d1ab02e8982d1bb8648
yschristian <yubahwesc@gmail.com>
2022-01-14 06:13:00 -0800

    edited

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const User = require('../models/User');
+
 module.exports.signup_get=(req,res)=>{
 
                    res.render('signup');
@@ -7,12 +9,19 @@ module.exports.login_get=(req,res)=>{
        res.render('login');
 }
 
-module.exports.signup_post=(req,res)=>{
-    res.send('new signup');
+module.exports.signup_post=  async(req,res)=>{
+         const{ email,password }=req.body
+       try{
+          const user = await User.create({ email , password });
+             res.status(201).json(user);
+       }catch(err){
+             console.log(err);
+             res.status(400).send('error,user not created');
+       }      
 }
-module.exports.login_post=(req,res)=>{
-   const{email,password}=req.body
-   console.log(email,password);
+module.exports.login_post= async (req,res)=>{
+         const{ email,password }=req.body
+
     res.send('new  login');
 }
 
