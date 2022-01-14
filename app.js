11c9743010519ac71def13de0af8f60bc9470fbd
yschristian <yubahwesc@gmail.com>
2022-01-14 02:12:40 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express=require('express')
+const mongoose = require('mongoose')
+const authRoutes =require('./routes/authRoutes')
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+
+
+//view engine
+app.set('view engine','ejs');
+//data base conection
+ const dbURI ="mongodb+srv://chris:chris@cluster0.k1npp.mongodb.net/customer-user?";
+ mongoose.connect(dbURI,{ useNewUrlParser: true, useUnifiedTopology: true })
+ .then((result) => console.log('db conected'))
+ .catch((err) => console.log(err));
+
+ app.listen(3000);
+//routes
+ app.get('/',(req,res)=>{res.render('home')});
+ app.get('/smoothies',(req,res)=>{res.render('smoothies')});
+ app.use(authRoutes);
+
+
+   
