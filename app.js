e31ee65426949a8d6dc5e5a4e3301c0f4b78313e
yschristian <yubahwesc@gmail.com>
2022-01-17 13:45:33 -0800

    edit

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express=require('express')
 const mongoose = require('mongoose')
 const authRoutes =require('./routes/authRoutes')
-
+const cookieParser = require('cookie-parser')
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
-
+app.use(cookieParser());
 
 //view engine
 app.set('view engine','ejs');
@@ -26,10 +26,18 @@ app.set('view engine','ejs');
 //cookie
 app.get('/set-cookies',(req,res)=>{
      
-    res.setHeader('Set-Cookie','newUser=true');
+    // res.setHeader('Set-Cookie','newUser=true');
+//maxAge is session
+    res.cookie('newUser',false);
+    //secure:true means present in secure connection
+    //https0ly:accessed in backend only
+    //maxAge:used to set session
+    res.cookie('isEmployee',true,{maxAge:1000*60*60*24,httpOnly:true})
     res.send('i  got cookie');
 
 });
 app.get('/read-cookie',(req,res)=>{
-
+      const cookies=req.cookies;
+      console.log(cookies.newUser);
+      res.json(cookies);
 })
